141eb8c356a61b5546401f8c4ee386c9a9996c24
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-21 13:55:22 -0500

    ready for german :)

---
+++ chinese.js
@@ -0,0 +1,14 @@
+var theMover = (function chineseModule(obj) {
+  var chinese = {
+    "happy": "快乐",
+    "grandparent's": "祖父",
+    "day": "母节",
+    "i": "我",
+    "hope": "希望",
+    "assisted": "养",
+    "living": "老院",
+    "is": "是",
+    "awesome": "好"
+  }
+
+})(theMover);
