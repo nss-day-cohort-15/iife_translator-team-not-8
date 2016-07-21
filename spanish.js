04e917b60ff94c4aa3c01e4831eee73f4ffb3b54
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-07-21 13:54:55 -0500

    created theMover added styling

---
+++ spanish.js
@@ -0,0 +1,17 @@
+
+var theMover = (function germanModule(obj){
+  var spanish = {
+  'happy':'contento',
+  "grandparent's" : 'abuelos',
+  'day' : 'dia',
+  'hope' : 'esperanza',
+  'assisted' :'asistida',
+  'living' : 'vivo',
+  'is' : 'es',
+  'awesome' : 'increible'
+}
+
+})(theMover);
+
+
+
